465f2fb92371d0fb5c9df0d4ca758c17a463f5ec
dsochamp <143166247+dsochamp@users.noreply.github.com>
2024-07-23 16:48:36 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import urllib.request
 from bs4 import BeautifulSoup
-
-
 def dictionary():
     try:
         # This prepares the URL webscrape
@@ -12,24 +10,21 @@ def dictionary():
 
         # Starting the process
         htmlParse = BeautifulSoup(html, 'html.parser')
-        synonyms = []
+        synonyms, result = []
 
         # This webscrapes all the list elements of the webpage
         for para in htmlParse.find_all("li"):
             synonyms.append(para.get_text())
-        result = []
-
+            
         # This picks out the meaning part of the page
         index = 29
         while index < 31:
             result.append(synonyms[index])
             index = index + 1
-
         # This finsishes it off for the interface
         for i in result:
             print(f"\n{word} means - " + i)
-
-
+    # For detecting errors        
     except urllib.request.HTTPError:
         print("\nSorry, the word you typed is not in the dictionary")
 dictionary()
